fb78a94b9df4305060aeac1ff167ac8b5056e471
phadziewicz <you@example.com>
2021-05-19 10:34:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# zad1
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# z = np.linspace(0, 2 * np.pi, 100)
+# x = np.sin(z)
+# y = 2 * np.cos(z)
+# ax.plot(x, y, z, label='zadanie 1')
+# plt.legend()
+# plt.show()
+
+# zad2
+# s1 = pd.Series(np.random.randn(10))
+# s2 = pd.Series(np.random.randn(10))
+# s3 = pd.Series(np.random.randn(10))
+# s4 = pd.Series(np.random.randn(10))
+# s5 = pd.Series(np.random.randn(10))
+# plt.plot(s1, 'bo', label='s1')
+# plt.plot(s2, 'r1', label='s2')
+# plt.plot(s3, 'gs', label='s3')
+# plt.plot(s4, 'y*', label='s4')
+# plt.plot(s5, 'mp', label='s5')
+# plt.legend()
+# plt.show()
